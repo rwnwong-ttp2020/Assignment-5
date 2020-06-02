c9f69899175cac0ac3ada3fa47a3bae06982b005
rwnwong-ttp2020 <rwnwong@gmail.com>
2020-06-02 00:47:03 -0400

    Changed html file to include buttons. Completed functions addRow and addColumn.

---
+++ script.js
@@ -0,0 +1,52 @@
+let column = 1;
+
+function addRow () {
+    
+    let table = document.getElementById ("table");
+    let row = document.createElement ("tr");
+    table.appendChild (row);
+    for (let i = 0; i < column; i++) {
+        let cell = document.createElement ("td");
+        row.append (cell);
+        cell.textContent = "Hello";
+    }
+
+}
+
+function addColumn () {
+    let table = document.getElementById ("table");
+    let rows = Array.from (table.children);
+    for (let i = 0; i < rows.length; i++) {
+        let cell = document.createElement ("td");
+        rows[i].append (cell);
+        cell.textContent = "Hello";
+    }
+    column +=1;
+}
+
+
+
+
+
+/*let newRow = document.createElement("row");
+let a = newRow.insertCell (0);
+let b = newRow.insertCell (1);
+a.innerHTML = "new row, 0";
+b.innerHTML = "new row, 1";
+(document.querySelector("tr")).appendChild(newRow);
+
+
+
+
+
+function insert_Row()
+{
+var x=document.getElementById('sampleTable').insertRow(0);
+var y = x.insertCell(0);
+var z = x.insertCell(1);
+y.innerHTML="New Cell1";
+z.innerHTML="New Cell2";
+}
+*/
+
+//node.appendChild(newNode);
